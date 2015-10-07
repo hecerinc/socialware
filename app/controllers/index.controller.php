79719fdef09abd1da1aed10c6e464fd9b2fffe28
Hector Rincon <hecerinc@hotmail.com>
2015-10-07 15:59:32 -0500

    Removed dump info.

--- app/controllers/index.controller.php
+++ app/controllers/index.controller.php
@@ -3,15 +3,6 @@
 //GET route
 $app->get('/', function () use ($app) {
 	$app->render('index.html.twig');
-	$env = $app->environment();
-	unset($env['properties']['slim.flash']['app']['container']['data']['settings']['view']);
-	ob_start();
-	var_dump($env);
-	$dump = ob_get_clean();
-	// $self = $_SERVER['PHP_SELF'];
-	file_put_contents('./uridump2.txt', $dump);
-	// var_dump($self); echo "<br>";
-	// var_dump(dirname($self));
 })->name('home');
 
 $app->get('/acercade', function () use ($app) {
