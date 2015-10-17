bbaa91cef91bf56a75f2718faf641c35855926ab
jlogzz <juan_lorenzo_@hotmail.com>
2015-10-17 12:06:08 -0500

    Init de CRUD para admin-users

--- app/controllers/admin.controller.php
+++ app/controllers/admin.controller.php
@@ -25,4 +25,32 @@ $app->group('/admin', $authenticate($app, 'admin'), function() use ($app){ // Ad
 	//PATCH
 });
 
+$app->group('/admin/users', $authenticate($app, 'admin'), function() use ($app){ // Add authentication middleware
+	//GET
+	$app->get('/', function() use ($app){
+		//TODO Get all users
+	})->name('admin_all_users');
+	
+	$app->get('/new', function() use ($app){
+		//TODO Get all users
+	})->name('admin_new_user');
+	
+	$app->get('/:id', function($id) use ($app){
+		//TODO Get user by id
+	})->name('admin_get_user');
+	
+	//POST
+	$app->post('/:id', function($id) use ($app){
+		//TODO Update user
+	})->name('admin_update_user');
+	
+	$app->post('/new', function() use ($app){
+		//TODO Update user
+	})->name('admin_create_user');
+	
+	$app->post('/delete/:id', function($id) use ($app){
+		//TODO Delete user
+	})->name('admin_delete_user');
+});
+
 ?>
